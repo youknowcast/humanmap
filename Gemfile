d404162781f6d988c4bc8c495d463d35d9167fa0
Hideki Ambe <ykc@daycrift.net>
2025-04-16 01:31:43 +0900

    initialize

---
+++ Gemfile
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+source 'https://rubygems.org'
+
+ruby file: '.ruby-version'
---
+++ lib/human_map.rb
@@ -0,0 +1,48 @@
+require 'yaml'
+require 'erb'
+
+class HumanMap
+  def initialize
+    @humans = load_data
+    @start_year = -100  # BC100年から
+    @end_year = 2024    # 現代まで
+    @century_step = 100 # 100年ごとの区切り
+    @px_per_century = 300 # 1世紀あたりのピクセル数
+    @bar_padding = 20 # バーの左右のパディング合計（10px × 2）
+  end
+
+  def generate
+    template = File.read(File.join(__dir__, '../templates/index.html.erb'))
+    renderer = ERB.new(template)
+    output = renderer.result(binding)
+
+    File.write('index.html', output)
+  end
+
+  private
+
+  def load_data
+    YAML.load_file(File.join(__dir__, '../data/human.yaml'))
+  end
+
+  def timeline_years
+    (@start_year..@end_year).step(@century_step).to_a
+  end
+
+  def year_to_position(year)
+    # マイナスの年（BC）も適切に処理
+    year_value = year.to_s.sub('?', '').to_i
+    ((year_value - @start_year) * @px_per_century / @century_step).round
+  end
+
+  def total_width
+    total_years = @end_year - @start_year
+    (total_years * @px_per_century / @century_step).round
+  end
+
+  # バーの幅を計算（パディングを考慮）
+  def calculate_bar_width(birth_year, death_year)
+    width = year_to_position(death_year) - year_to_position(birth_year)
+    width - @bar_padding # パディング分を引く
+  end
+end
